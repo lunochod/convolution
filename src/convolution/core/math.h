cdfb6fb9374542bb1caf5d016103d38727e20d91
lunochod <lunochod@users.noreply.github.com>
2020-08-06 23:29:24 -0400

    merge MatrixMultiplication.h and MatrixTranspose.h into math.h

--- src/convolution/core/math.h
+++ src/convolution/core/math.h
@@ -1,5 +1,5 @@
-#ifndef CONVOLUTION_CORE_MATRIXMULTIPLICATION_H
-#define CONVOLUTION_CORE_MATRIXMULTIPLICATION_H
+#ifndef CONVOLUTION_CORE_MATH_H
+#define CONVOLUTION_CORE_MATH_H
 
 #include <convolution/core/logging.h>
 
@@ -110,7 +110,27 @@ bool mult(uint32_t M, uint32_t N, uint32_t K, T *c, const T *a, const T *b) {
   }
 }
 
+/// simple out-of-place matrix transpose for matrices M != N
+/// faster algorithms exist
+template <typename T, MatrixOrder order>
+void transpose(uint32_t M, uint32_t N, T *data, T *buffer) {
+  if constexpr (order == core::MatrixOrder::kRowMajor) {
+    for (uint32_t m = 0; m < M; ++m) {
+      for (uint32_t n = 0; n < N; ++n) {
+        buffer[n * M + m] = data[m * N + n];
+      }
+    }
+  } else {
+    for (uint32_t n = 0; n < N; ++n) {
+      for (uint32_t m = 0; m < M; ++m) {
+        buffer[m * N + n] = data[n * M + m];
+      }
+    }
+  }
+  memcpy(data, buffer, sizeof(T) * M * N);
+}
+
 }  // namespace core
 }  // namespace convolution
 
-#endif  // CONVOLUTION_CORE_MATRIXMULTIPLICATION_H
+#endif  // CONVOLUTION_CORE_MATH_H
